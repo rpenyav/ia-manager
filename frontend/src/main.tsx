e2865feae379855a2e0b4088e70dba2dc5662813
Rafa <rafa@wad.cat>
2026-02-09 23:26:55 +0100

    Remove jquery bootstrap shim

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import $ from 'jquery';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
-if (typeof window !== 'undefined') {
-  const anyWindow = window as any;
-  anyWindow.jQuery = $;
-  anyWindow.$ = $;
-  if (!(($ as any).isArray)) {
-    ($ as any).isArray = Array.isArray;
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
